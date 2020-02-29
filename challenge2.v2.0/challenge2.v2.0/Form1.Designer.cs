44b9009a5c1b86229dd45758a50e5008be3a558d
xristoforoskasimatis <xristoforoskasimatis@gmail.com>
2020-02-29 21:28:34 +0100

    iniitial commit

---
+++ challenge2.v2.0/challenge2.v2.0/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace challenge2.v2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(170, 386);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "----";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(168, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ellapsed time (ms):";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(225, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 192);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(225, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 34);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "n=";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(150, 394);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBOUTToolStripMenuItem,
+            this.hELPToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(460, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.aBOUTToolStripMenuItem.Text = "&ABOUT";
+            this.aBOUTToolStripMenuItem.Click += new System.EventHandler(this.aBOUTToolStripMenuItem_Click);
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.hELPToolStripMenuItem.Text = "&HELP";
+            this.hELPToolStripMenuItem.Click += new System.EventHandler(this.hELPToolStripMenuItem_Click);
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.eXITToolStripMenuItem.Text = "&EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 449);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "challenge2v2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+    }
+}
+
---
+++ challenge2.v2.0/challenge2.v2.0/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace challenge2.v2._0
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;// otan anoigei i forma epilegmeno control einai to textbox
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int z;
+            if (int.TryParse(textBox1.Text, out z) == true)//elegxos an einai integer
+            {
+                richTextBox1.Clear();//an nai adeiazei to textbox kai 
+                z = int.Parse(textBox1.Text);//dinei timi
+            }
+            int[] pin = new int[z];//dimiourgei pinaka megeyhous isou me ton arthmo pou evale o xristis
+            List<int> n = new List<int>();//dimiourgia listas integer
+            Stopwatch watch = new Stopwatch();//dimiourgia rologia gia metrisi
+            watch.Start();//enerksi xronou
+            for (int i = 0; i < z;)//enarksi epanalipsis gia ola ta stixia apo to 1-z
+            {
+            label1: int ra = r.Next(1, z + 1);//tixaios ari8mos sto diastima [1,z]
+                if (n.Contains(ra))//an periexetai stin lista
+                {
+                    goto label1;//ksanaepilegei tixaio ari8mo
+                }
+                else
+                {
+                    n.Add(ra);//allios ton prosthetei stin lista
+                    pin[i] = ra;  //ton topothetei stin thesi tou pinaka                 
+                    i++;//kai ayksanei tin epanalipsi
+                }
+            }
+            watch.Stop();//stamataei to roloi
+            label3.Text = watch.ElapsedMilliseconds.ToString();//emfanisi xronou
+            for (int y = 0; y < z; y++)
+                richTextBox1.AppendText(pin[y] + Environment.NewLine);//emfanisei stoixewn pinaka
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)//an pati8ei enter
+            {
+                button1_Click(sender, e);//klisi sinartisis gia to koumpi
+            }
+        }
+
+        private void aBOUTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Δημιουργός: ΚΑΣΙΜΑΤΗΣ ΧΡΙΣΤΟΦΟΡΟΣ-ΝΙΚΗΤΑΣ." + Environment.NewLine +
+                "Τμήμα πληροφορικής, Πανεπιστημίο Πειραιά");//emfanisi dimiourgou
+        }
+
+        private void hELPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ΒΑΛΕ ΕΝΑΝ ΑΚΕΡΑΙΟ ΣΤΟ ΠΕΔΙΟ ΕΙΣΑΓΩΓΗΣ. ΣΤΗΝ ΣΥΝΕΧΕΙΑ ΠΑΤΗΣΕ ΤΟ ΚΟΥΜΠΙ ΚΑΙ " +
+                "ΘΑ ΕΜΦΑΝΙΣΤΟΥΝ ΣΤΟ ΑΡΙΣΤΕΡΟ ΜΕΡΟΣ ΤΗΣ ΟΘΟΝΗΣ ΟΛΟΙ ΑΡΙΘΜΟΙ ΑΠΟ ΤΟ 1 ΜΕΧΡΙ ΤΟΝ ΑΡΙΘΜΟ ΠΟΥ ΕΒΑΛΕΣ" +
+                "ΜΕ ΤΥΧΑΙΑ ΣΕΙΡΑ.ΤΕΛΟΣ ΕΜΦΑΝΙΖΕΤΑΙ Ο ΧΡΟΝΟΣ ΠΟΥΥ ΧΡΕΙΑΣΤΗΚΕ ΓΙΑ ΝΑ ΟΛΟΚΛΗΡΩΘΕΙ Η ΔΙΑΔΙΚΑΣΙΑ. ");
+            // emfanisi voitheias
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("ARE YOU SURE YOU WANT TO EXIT?", "EXIT", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {//emfanisi minimatos gia klisimo
+                //an o xristis patisie nai kleinei
+
+                this.Close();
+            }
+        }
+    }
+}
